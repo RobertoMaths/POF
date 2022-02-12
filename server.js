9cae702a15d11e1365e5abf694338fc805ff1cf6
RobertoMaths <charmanderroberto124@gmail.com>
2022-02-12 18:58:04 -0500

    Rutas y ejs

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 const express = require("express");
+const path = require("path");
+const MainRouter = require("./rutas/main");
 const server = express();
 
 // Configuración
 
 server.set("port", 3000);
+server.set("views",path.join(__dirname,"vistas"))
+server.engine("html",require("ejs").renderFile)
+server.set("view engine","ejs")
+
+// Middlewares
+
+server.use(express.static(__dirname + "/public"));
+server.use(express.json());
 
 // Rutas
 
-server.get("/", (req, res)=> {
-    res.json({
-        server: "Server funcionando"
-    })
-})
+server.use(MainRouter);
 
 // Server escuchando
 
-server.listen(server.get("port"), ()=> console.log("Server en puerto: ", server.get("port")))
+server.listen(server.get("port"), ()=> {
+    console.log(`Server en puerto: ${server.get("port")}`);
+})
